b7980bfd97065ccfbf5dbd49b68fd00e818887c8
Meet Solanki <meetreet18@gmail.com>
2025-01-25 22:17:33 +0530

    data ingestion

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from src.projectAlpha import logger
 
-logger.info("welcome to our custom logging datascience")
+from src.projectAlpha.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+
+STAGE_NAME="data ingestion"
+
+try:
+    logger.info(f">>>> stage {STAGE_NAME} started  <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.initiate_data_ingestion()
+    logger.info(f">>>> stage {STAGE_NAME} completed  <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
--- src/projectAlpha/utils/common.py
+++ src/projectAlpha/utils/common.py
@@ -3,13 +3,13 @@ import yaml
 from src.projectAlpha import logger
 import json
 import joblib
-from ensure import ensure_anotations
+from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
 from typing import Any
 from box.exceptions import BoxValueError
 
-@ensure_anotations
+@ensure_annotations
 def read_yaml(path_to_yaml: Path)-> ConfigBox:
     try:
         with open(path_to_yaml) as yaml_file:
@@ -22,7 +22,7 @@ def read_yaml(path_to_yaml: Path)-> ConfigBox:
         raise e
 
 
-@ensure_anotations
+@ensure_annotations
 def create_directories(path_to_directories: list,verbose = True):
     for path in path_to_directories:
         os.makedirs(path,exist_ok=True)
@@ -30,14 +30,14 @@ def create_directories(path_to_directories: list,verbose = True):
             logger.info(f"created directory as {path}")
 
 
-@ensure_anotations
+@ensure_annotations
 def save_json(path: Path, data: dict):
     with open(path,"w") as f:
         json.dump(data,f,indent=4)
     logger.info(f"json file saved at: {path}")
 
 
-@ensure_anotations
+@ensure_annotations
 def load_json(path: Path)->ConfigBox:
     with open(path) as f:
         content = json.load(f)
@@ -45,13 +45,13 @@ def load_json(path: Path)->ConfigBox:
     return ConfigBox(content)
 
 
-@ensure_anotations
+@ensure_annotations
 def save_bin(data: Any, path: Path):
     joblib.dump(value=data,filename=path)
     logger.info(f"binary file saved as {path}")
 
 
-@ensure_anotations
+@ensure_annotations
 def load_bin(path: Path)-> Any:
     data = joblib.load(path)
     logger.info(f"binary file loaded from: {path}")
